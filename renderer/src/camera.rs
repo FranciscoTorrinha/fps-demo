09018c5a3849e25aa69f51669b63a8946c11ba4d
Francisco Torrinha <ftorrinha.dev@gmail.com>
2023-11-13 22:40:33 +0000

    Camera: Added basic camera math

---
+++ renderer/src/camera.rs
@@ -0,0 +1,36 @@
+use nalgebra::{Isometry3, Matrix4, Perspective3, Point3, Vector3};
+
+use crate::WindowDimensions;
+
+pub struct Camera {
+    projection: Matrix4<f32>,
+    view: Matrix4<f32>,
+}
+
+impl Camera {
+    pub fn with_perpective(
+        window_dimensions: WindowDimensions,
+        position: Point3<f32>,
+        look_at: Point3<f32>,
+    ) -> Self {
+        let up = Vector3::new(0.0, 1.0, 0.0);
+        let view = Isometry3::look_at_lh(&position, &look_at, &up);
+
+        Self {
+            projection: Perspective3::new(
+                window_dimensions.width as f32 / window_dimensions.height as f32,
+                45.0,
+                0.01,
+                1000.0,
+            )
+            .as_matrix()
+            .clone(),
+
+            view: view.into(),
+        }
+    }
+
+    pub fn calculate_mvp(&self, model: Matrix4<f32>) -> Matrix4<f32> {
+        self.projection * self.view * model
+    }
+}
--- renderer/src/lib.rs
+++ renderer/src/lib.rs
@@ -1,9 +1,11 @@
+mod camera;
 mod common;
 mod primitives;
 mod render_pass_exec;
 mod renderer;
 mod rendering_context;
 
+pub use camera::Camera;
 pub use common::{GenericVertex, WindowDimensions};
 pub use primitives::TrianglePrimitive;
 pub use render_pass_exec::RenderPassExecutor;
